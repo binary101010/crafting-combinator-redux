45afb68a91405cbe277cf98924871f07d5b29a55
binary101010 <binary101010@live.de>
2018-04-21 19:20:25 +0200

    Mod Folder Bootstrap

---
+++ crafting-combinator-redux/control.lua
@@ -0,0 +1,12 @@
+-- Global Event registration goes here...
+
+-- eg. :
+  
+--script.on_event(defines.events.on_built_entity, events.on_built)
+--script.on_event(defines.events.on_robot_built_entity, events.on_robot_built)
+
+--script.on_event(defines.events.on_pre_player_mined_item, events.on_destroyed)
+--script.on_event(defines.events.on_robot_pre_mined, events.on_destroyed)
+--script.on_event(defines.events.on_entity_died, events.on_destroyed)
+
+--script.on_event(defines.events.on_tick, events.on_tick)
